61daa6f4064ee2901bbfc8f679b5582b2038ad23
seimiyaa <143976419+seimiyaa@users.noreply.github.com>
2023-11-09 15:31:19 +0900

    リンク先に情報を追加する

--- app.js
+++ app.js
@@ -9,13 +9,60 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mysql = require("mysql2");
-
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "rootroot",
   database: "express_db",
 });
+
+const users = [
+  {
+    id: 1,
+    name: "鈴木 さくら",
+    hiragana: "すずき さくら",
+    address: "東京都渋谷区桜丘町1-2-3",
+    phoneNumber: "03-1234-5678",
+    startDate: "2023年10月15日",
+    endDate: "2023年10月17日",
+  },
+  {
+    id: 2,
+    name: "山田 たけし",
+    hiragana: "やまだ たけし",
+    address: "大阪府大阪市中央区花園町4-5-6",
+    phoneNumber: "06-9876-5432",
+    startDate: "2023年11月10日",
+    endDate: "2023年11月20日",
+  },
+  {
+    id: 3,
+    name: "田中 ゆりこ",
+    hiragana: "たなか ゆりこ",
+    address: "京都府京都市東山区鴨川町7-8-9",
+    phoneNumber: "075-1111-2222",
+    startDate: "2023年10月5日",
+    endDate: "2023年10月9日",
+  },
+  {
+    id: 4,
+    name: "高橋 はるか",
+    hiragana: "たかはし はるか",
+    address: "福岡県福岡市博多区中洲1-2-3",
+    phoneNumber: "092-3333-4444",
+    startDate: "2023年9月20日",
+    endDate: "2023年9月25日",
+  },
+  {
+    id: 5,
+    name: "佐藤 けんた",
+    hiragana: "さとう けんた",
+    address: "北海道札幌市中央区大通西1-2-3",
+    phoneNumber: "011-5555-6666",
+    startDate: "2023年10月12日",
+    endDate: "2023年10月17日",
+  },
+];
 
 app.get("/", (req, res) => {
   const sql = "SELECT * FROM vehicles";
@@ -39,12 +86,13 @@ app.get("/create", (req, res) => {
   res.sendFile(path.join(__dirname, "html/form.html"));
 });
 
-app.get("/edit/:id", (req, res) => {
-  const sql = "SELECT * FROM vehicles WHERE id = ?";
+app.get("/edit/:car_model", (req, res) => {
+  const sql = "SELECT * FROM vehicles WHERE car_model = ?";
   con.query(sql, [req.params.id], function (err, result, fields) {
     if (err) throw err;
     res.render("edit", {
       vehicles: result[0],
+      users: users,
     });
   });
 });
